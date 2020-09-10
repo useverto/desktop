6aeb5c5984daed6b772cdc9982dddf6e320d6ab0
divy-work <dj.srivastava23@gmail.com>
2020-09-10 18:28:19 +0530

    feat: add windows installer

---
+++ install/windows.ps1
@@ -0,0 +1,52 @@
+$ErrorActionPreference = 'Stop'
+
+if ($args.Length -eq 1) {
+  $v = $args.Get(0)
+}
+
+$VertoInstall = $env:VERTO_INSTALL
+$BinDir = if ($VertoInstall) {
+  "$VertoInstall"
+} else {
+  "$Home\.verto"
+}
+
+$VertoZip = "$BinDir\desktop.zip"
+$VertoExe = "$BinDir\desktop.exe"
+
+# GitHub requires TLS 1.2
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+$VertoUri = if (!$v) {
+  "https://github.com/useverto/desktop/releases/latest/download/verto-x64-windows.zip"
+} else {
+  "https://github.com/useverto/desktop/releases/download/${v}/verto-x64-windows.zip"
+}
+
+if (!(Test-Path $BinDir)) {
+  New-Item $BinDir -ItemType Directory | Out-Null
+}
+
+Invoke-WebRequest $VertoUri -OutFile $VertoZip -UseBasicParsing
+
+if (Get-Command Expand-Archive -ErrorAction SilentlyContinue) {
+  Expand-Archive $VertoZip -Destination $BinDir -Force
+} else {
+  if (Test-Path $VertoExe) {
+    Remove-Item $VertoExe
+  }
+  Add-Type -AssemblyName System.IO.Compression.FileSystem
+  [IO.Compression.ZipFile]::ExtractToDirectory($VertoZip, $BinDir)
+}
+
+Remove-Item $VertoZip
+
+$User = [EnvironmentVariableTarget]::User
+$Path = [Environment]::GetEnvironmentVariable('Path', $User)
+if (!(";$Path;".ToLower() -like "*;$BinDir;*".ToLower())) {
+  [Environment]::SetEnvironmentVariable('Path', "$Path;$BinDir", $User)
+  $Env:Path += ";$BinDir"
+}
+
+Write-Output "Verto Desktop was installed successfully to $VertoExe"
+Write-Output "Run 'desktop.exe' to get started"
