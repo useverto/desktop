6be99948c009e3b95161cddcf0d08bd328365012
divy-work <dj.srivastava23@gmail.com>
2020-09-11 19:03:19 +0530

    feat: setup autorestart

--- desktop.go
+++ desktop.go
@@ -3,19 +3,32 @@ package main
 import (
 	"log"
 
-	"github.com/useverto/desktop/webview"
-
+	"github.com/mitchellh/go-homedir"
 	_ "github.com/useverto/desktop/bundle"
+	"github.com/useverto/desktop/webview"
 )
 
 func main() {
 	log.Println("Starting thread loop")
 	// init
 	debug := true
+	_, err := setupWatcher()
 
+	if err != nil {
+		// do something sensible
+		log.Fatal(err)
+	}
+
+	// upgrade the desktop version
 	if NeedsUpgrade() {
 		log.Printf("%s is not latest, upgrading...", version)
-		DownloadRelease(".")
+		// determine download location
+		downloadLoc, _ := homedir.Expand(`~/.verto_desktop`)
+		// download latest release from github
+		DownloadRelease(downloadLoc)
+		// unzip the release zip
+		NewUnzip(downloadLoc, downloadLoc).Extract()
+
 	}
 
 	// start the server with website source
--- go.mod
+++ go.mod
@@ -3,10 +3,12 @@ module github.com/useverto/desktop
 go 1.14
 
 require (
+	github.com/fsnotify/fsnotify v1.4.9
 	github.com/google/go-github v17.0.0+incompatible // indirect
 	github.com/google/go-querystring v1.0.0 // indirect
 	github.com/hashicorp/go-version v1.2.1 // indirect
-	github.com/mitchellh/go-homedir v1.1.0 // indirect
+	github.com/kardianos/osext v0.0.0-20190222173326-2bc1f35cddc0
+	github.com/mitchellh/go-homedir v1.1.0
 	github.com/rakyll/statik v0.1.7
 	github.com/tcnksm/go-latest v0.0.0-20170313132115-e3007ae9052e
 	golang.org/x/net v0.0.0-20200904194848-62affa334b73 // indirect
