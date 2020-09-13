953569cb295df8ca867d39699b76fcb0cfcda4ec
divy-work <dj.srivastava23@gmail.com>
2020-09-13 19:50:08 +0530

    feat: add linux debian

---
+++ debian/entry.sh
@@ -0,0 +1,11 @@
+echo "
+[Desktop Entry]
+Terminal=false
+Exec=/usr/bin/desktop
+Icon=/usr/bin/verto_desktop.png
+Type=Application
+Categories=Graphics;
+StartupNotify=true
+Name=Verto Desktop
+GenericName=Verto
+" >> /usr/share/applications/verto_desktop.desktop
