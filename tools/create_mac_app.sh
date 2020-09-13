dcbb90480175d7cc812f14f9ef9be53a11691416
divy-work <dj.srivastava23@gmail.com>
2020-09-13 13:04:05 +0530

    feat: add mac app script

---
+++ tools/create_mac_app.sh
@@ -0,0 +1,16 @@
+# creates the following directory structure
+# verto_desktop.app
+# └── Contents
+#     ├── Info.plist
+#     ├── MacOS
+#     |    └── verto_desktop
+#     └── Resources
+#         └── verto_desktop.icns
+
+mkdir verto_desktop.app
+mkdir verto_desktop.app/Contents
+mkdir verto_desktop.app/Contents/MacOS
+mkdir verto_desktop.app/Contents/Resources
+cp desktop verto_desktop.app/Contents/MacOS/
+cp Info.plist verto_desktop.app/
+cp assets/verto_desktop.icns verto_desktop.app/Contents/Resources/
