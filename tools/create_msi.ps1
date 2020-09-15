98fe4a0931b7c85c2a7b91cd69a3f735174350f4
divy-work <dj.srivastava23@gmail.com>
2020-09-15 13:17:09 +0530

    chores: refract ps script to CI

--- tools/create_msi.ps1
+++
@@ -1,4 +0,0 @@
-# compile each .wxs into a .wixobj object using candle.exe
-exec { &"candle.exe" -out "Verto.wixobj" "Verto.wxs" } 
-# link all the .wixobj objects into an MSI installer using light.exe
-exec { &"light.exe" -b "./desktop.exe" "Verto.wixobj" -out "Verto.msi" }
